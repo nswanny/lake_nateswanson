cae6e65742a74e719452af21504439fab703ed27
Swanson <Nathaniel.Swanson@uj.edu>
2020-02-05 11:39:44 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Image from assets"),
+        ),
+        body: Image.asset('assets/images/lake.jpg'), //   <-- image
+      ),
+    );
+  }
+}
